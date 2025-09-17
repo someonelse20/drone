fc2dba276a54f510bc6868fbbecb3723b3b5a7d5
someonelse20 <kennethmcclung@gmail.com>
2025-09-17 19:08:47 -0400

    testing to make sure to remove build dirs

--- code/old-flight-controller/build/CMakeFiles/driver.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libdriver.a"
-  "libdriver.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- code/old-flight-controller/build/CMakeFiles/fusion.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libfusion.a"
-  "libfusion.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
