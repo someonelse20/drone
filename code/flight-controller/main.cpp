f7964beb1cae00f7fbb27df54de2fc8b96a7e8ef
drone pi <drone@example.com>
2026-01-03 20:31:30 +0000

    cleaned up mag calibration

--- code/flight-controller/main.cpp
+++ code/flight-controller/main.cpp
@@ -122,11 +122,16 @@ int main() {
 
 	ahrs_alg.settings.mag_calibrate.soft_iorn = {{ 0.90782961, -0.05349811,  0.0207239 },
                                                  {-0.11265483, -0.91579853,  0.02113201},
-                                                 {-0.06981889, -0.12123726,  0.91697565}};
+                                                 { 0.06981889, -0.12123726, -0.91697565}};
 	ahrs_alg.settings.mag_calibrate.hard_iorn = {-0.00618111, -0.12120257, -0.15837325};
+	ahrs_alg.settings.mag_calibrate.rotation_matrix = {{ 0,  1,  0},
+                                                       {-1,  0,  0},
+                                                       { 0,  0,  1}};
+	/*
 	ahrs_alg.settings.mag_calibrate.rotation_matrix = {{ 1,  0,  0},
                                                        { 0, -1,  0},
                                                        { 0,  0, -1}};
+	*/
 
 	ahrs_alg.settings.accel_rejection_t = 0.5;
 
